e243499c4d4008abc1c8ef2c982ffac14f8d131d
Richard Foster <richard@madetech.co.uk>
2015-09-27 14:45:14 +0100

    Add full stop to wiserob output

--- test/wisdom_test.js
+++ test/wisdom_test.js
@@ -9,7 +9,7 @@ describe('Wisdom', function () {
 
   it('the returned function should give a random quote when called', function () {
     let wisdom = Wisdom(['to be happy'], ['to be healthy', 'to be unhealthy']);
-    let possibilities = ['to be happy is to be healthy', 'to be happy is to be unhealthy'];
+    let possibilities = ['to be happy is to be healthy.', 'to be happy is to be unhealthy.'];
     expect(possibilities).to.include(wisdom());
   });
 });

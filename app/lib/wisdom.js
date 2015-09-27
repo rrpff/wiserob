5fbc50b2872e4b4c92167fcadecc2d7e4cda27a3
Richard Foster <richard@madetech.co.uk>
2015-09-27 01:21:57 +0100

    Add "wisdom" logic

---
+++ app/lib/wisdom.js
@@ -0,0 +1,9 @@
+function pick (array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+module.exports = function (left, right) {
+  return function wisdom () {
+    return pick(left) + ' is ' + pick(right);
+  }
+}

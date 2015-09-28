955b0804be8d8131f6840b26d0c83ffd467ca71c
Richard Foster <richard@madetech.co.uk>
2015-09-28 22:53:23 +0100

    Add quotes when clicking on the body

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,14 +1,20 @@
 const React = require('react');
 const QuotePage = require('./quotes/quote-page');
 const { connect } = require('react-redux');
+const { addQuote } = require('../actions/quotes');
 
 class App extends React.Component {
   render () {
     return (
-      <div className="wrapper">
+      <div className="wrapper" onClick={e => this.handleClick(e)}>
         <QuotePage quotes={this.props.quotes} />
       </div>
     );
+  }
+
+  handleClick (e) {
+    if (this.props.onClick) this.props.onClick();
+    this.props.dispatch(addQuote());
   }
 }
 

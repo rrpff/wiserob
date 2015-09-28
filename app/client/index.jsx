ae89d27fb7844b19913cf2a7b11b3ed3ed91da56
Richard Foster <richard@madetech.co.uk>
2015-09-28 22:15:55 +0100

    Render directly on the body

--- app/client/index.jsx
+++ app/client/index.jsx
@@ -3,11 +3,9 @@ const { Provider } = require('react-redux');
 const App = require('../components/app');
 const store = require('./store');
 
-let wrapper = document.getElementsByClassName('wrapper')[0];
-
 React.render(
   <Provider store={store}>
     {() => <App />}
   </Provider>,
-  wrapper
+  document.body
 );
